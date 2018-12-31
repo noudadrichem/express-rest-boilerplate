80bd9a16d9e85efe3bd25cc74a2ccc48e2a80b16
info@noudadrichem.com <info@noudadrichem.com>
2018-12-31 16:17:21 +0100

    cleanpu

--- app/db.js
+++ app/db.js
@@ -5,14 +5,13 @@ import { database } from '../config'
 export default function initializeDatabaseConnectino({ log }) {
   mongoose.Promise = Promise
   const { username, password, host, port, name } = database
-  const URL = `mongodb://${username !== '' ? `${username || process.env.MONGODB_USER }:${password || process.env.MONGODB_PASS}@` : ''}${host}:${port}/${name}`
+  const generatedMongoUrl = `mongodb://${username !== '' ? `${username || process.env.MONGODB_USER }:${password || process.env.MONGODB_PASS}@` : ''}${host}:${port}/${name}`
+  const URL = process.env.MONGO_URL || generatedMongoUrl
 
-  const JA = process.env.MONGO_URL || URL
-  console.log({ JA })
   return new Promise((resolve, reject) => {
-    mongoose.connect(JA, { useMongoClient: true })
+    mongoose.connect(URL, { useMongoClient: true })
       .then(db => {
-        log.info('Database connected succesfully')
+        log.info(`Database connected succesfully to ${URL}`)
         resolve(db)
       }).catch(err => {
         log.error('Database connection failed', { err })
